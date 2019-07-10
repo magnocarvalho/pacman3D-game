23b0f4a2d088d0b0fd062b866f47f699db3a32c7
Magno Carvalho <magnocarv@hotmail.com>
2019-07-10 04:43:38 -0300

    game iniciado

---
+++ Assets/robo.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class robo : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject pc;
+    private NavMeshAgent agente;
+    public GameObject[] waypoints = new GameObject[4];
+    private int index = 0;
+    public float maxDist = 1;
+    private bool perseguindo = false;
+    void Start()
+    {
+        agente = GetComponent<NavMeshAgent>();
+        proximo();
+    }
+
+    // Update is called once per frame
+    void proximo()
+    {
+        agente.destination = waypoints[index++].transform.position;
+        if (index >= waypoints.Length)
+            index = 0;
+
+    }
+    void Update()
+    {
+        //agente.destination = pc.transform.position;
+        //Se a distância entre o NPC e o waypoint atual for menor que 1
+        //então, vá para o próximo destino
+        if (perseguindo || Vector3.Distance(transform.position, pc.transform.position) <= 5)
+        {
+            perseguindo = true;
+            agente.destination = pc.transform.position;
+        }
+        else if (Vector3.Distance(transform.position, agente.destination) <= maxDist)
+            proximo();
+
+    }
+}
---
+++ Assets/scriptPlayerRiberinho.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptPlayerRiberinho : MonoBehaviour
+{
+     public float velocidade;
+    public float velRot;
+    private Rigidbody rbd;
+    public Animation anim;
+    public   AnimationClip clip;
+    private Quaternion rotOriginal;
+    private float rotMX=0;    
+    // Start is called before the first frame update
+    void Start()
+    {
+        velRot = 40;
+        velocidade = 5;
+        rbd = GetComponent<Rigidbody>();
+              
+        rotOriginal = transform.localRotation;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveL = Input.GetAxis("Horizontal");
+        float moveF = Input.GetAxis("Vertical");
+
+        rotMX += Input.GetAxis("Mouse X");
+        
+        Quaternion lado = Quaternion.AngleAxis(rotMX,Vector3.up);
+        transform.localRotation = rotOriginal * lado ;
+
+        rbd.velocity = transform.TransformDirection(new Vector3(moveL * velocidade, rbd.velocity.y, moveF * velocidade));
+
+
+        int dir = 0;
+        if (Input.GetKey(KeyCode.Q))
+            dir = -1;
+        else if (Input.GetKey(KeyCode.E))
+            dir = 1;
+
+        transform.Rotate(new Vector3(0,dir * velRot * Time.deltaTime,0));
+       
+    }
+}
